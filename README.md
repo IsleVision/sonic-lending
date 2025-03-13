2d175b9f03f36d905fcb79132a2bb833310c37c9
Tao Song <tsong@kyros.ai>
2025-03-14 01:54:48 +0800

    fix borrow limit calculation (chainlink price feed)

--- README.md
+++ README.md
@@ -12,19 +12,19 @@
 
 ## Overview
 
-A decentralized lending and borrowing protocol for users to lend and borrow Sonic Asset. A completely trustless environment for both lenders and borrowers, built on top of Ethereum solutions. Lenders supply the protocol with Sonic Asset and in return earn interest in a form of $Sonk tokens, protocol’s governance token. Thanks to lenders, our protocol will have liquidity to supply the borrowers with Sonic Asset. In order for borrowers to borrow they will have to collateralize ETH, our protocol works in an over-collateralized model, meaning borrower’s ETH collateral amount > Sonic Asset amount they can borrow. The borrowers pay interest to the protocol in order to maintain it, the interest will be deducted from ETH collateral when the borrower borrows Sonic Asset. In case of liquidation, our protocol keeps the borrower's ETH collateral and rewards the liquidator.
+A decentralized lending and borrowing protocol for users to lend and borrow Sonic Asset. A completely trustless environment for both lenders and borrowers, built on top of Ethereum solutions. Lenders supply the protocol with Sonic Asset and in return earn interest in a form of $Sonk tokens, protocol’s governance token. Thanks to lenders, our protocol will have liquidity to supply the borrowers with Sonic Asset. In order for borrowers to borrow they will have to collateralize WETH, our protocol works in an over-collateralized model, meaning borrower’s WETH collateral amount > Sonic Asset amount they can borrow. The borrowers pay interest to the protocol in order to maintain it, the interest will be deducted from WETH collateral when the borrower borrows Sonic Asset. In case of liquidation, our protocol keeps the borrower's WETH collateral and rewards the liquidator.
 
 ## Deliverables
 
 ### Web App:
 
-- Collateralize ETH
+- Collateralize WETH
 - Withdraw Sonic Asset
 - Repay Sonic Asset
 - Explore the following information:
   - Protocol’s name
   - User’s wallet address.
-  - User’s ETH and $Sonk balance.
+  - User’s WETH and $Sonk balance.
   - Sonic Asset % APY
   - Current supplied or borrowed amounts, if any.
 
@@ -35,21 +35,21 @@ A decentralized lending and borrowing protocol for users to lend and borrow Soni
   - Mapping of lender to amount of Sonic Asset provided.
   - Mapping of lender to amount of $Sonk earned.
   - Mapping of borrower to amount of Sonic Asset borrowed.
-  - Mapping of borrower to amount of ETH collateralized.
+  - Mapping of borrower to amount of WETH collateralized.
   - Lend function - saves earned $Sonk amount, if any => updates lend position state => sends user’s provided amount of Sonic Asset to the protocol.
-  - Lender’s withdrawal function - saves amount of $Sonk earned, if any => resets lend position state => sends ETH to protocol.
+  - Lender’s withdrawal function - saves amount of $Sonk earned, if any => resets lend position state => sends WETH to protocol.
   - Claim Yield function - calculates earned $Sonk yield => mints the lender the amount.
-  - Collateralize ETH function - updates collateral position => sends ETH to protocol.
-  - Withdraw ETH function(Can’t be called while borrowing) - updates collateral position => sends ETH to user.
-  - Borrow function(Only if ETH is collateralized) - deduct protocol’s fees (0.3%) => updates borrow position => sends the user amount of Sonic Asset provided inside the borrow limit.
+  - Collateralize WETH function - updates collateral position => sends WETH to protocol.
+  - Withdraw WETH function(Can’t be called while borrowing) - updates collateral position => sends WETH to user.
+  - Borrow function(Only if WETH is collateralized) - deduct protocol’s fees (0.3%) => updates borrow position => sends the user amount of Sonic Asset provided inside the borrow limit.
   - Borrower’s repay function - updates borrow position state => sends Sonic Asset from borrower to protocol.
-  - Liquidate function(any user including the deployer can execute a liquidation function) - check if the liquidation point has been passed => liquidate borrower’s position => send the liquidator liquidation reward (1.25%) of borrower’s ETH collateral as reward.
-  - Calculate Price of ETH function - Using Chainlink’s price oracle calculates and returns the price of ETH in Sonic Asset.
+  - Liquidate function(any user including the deployer can execute a liquidation function) - check if the liquidation point has been passed => liquidate borrower’s position => send the liquidator liquidation reward (1.25%) of borrower’s WETH collateral as reward.
+  - Calculate Price of WETH function - Using Chainlink’s price oracle calculates and returns the price of WETH in Sonic Asset.
   - Calculate Yield Time - calculates the amount of time the lender has been lending.
   - Calculate Yield Total - calculates the amount of $Sonk tokens the lender has earned.
-  - Calculate Borrow limit function - get the price of ETH in
-  - Sonic Asset => (ETH price _ ETH collateral) _ borrow rate (70%) - borrower’s amount of borrowed Sonic Asset, if any .
-  - Calculate Liquidation point - get the price of ETH in Sonic Asset => amount of borrowed Sonic Asset + liquidation rate (10%).
+  - Calculate Borrow limit function - get the price of WETH from Chainlink's price feed
+  - Sonic Asset => (WETH price _ WETH collateral) _ borrow rate (70%) - borrower’s amount of borrowed Sonic Asset, if any .
+  - Calculate Liquidation point - get the price of WETH in Sonic Asset => amount of borrowed Sonic Asset + liquidation rate (10%).
 
 - SonkToken ($Sonk) contract:
 
--- contracts/ProtocolCore.sol
+++ contracts/ProtocolCore.sol
@@ -8,11 +8,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 ///@notice Main Sonk contract responsible for lending, collateralizing and borrowing
 ///@author John Nguyen (jooohn.eth)
-contract ProtocolCore is Ownable{
+contract ProtocolCore is Ownable {
 
     ///@notice events emitted after each action.
     event Lend(address indexed lender, uint amount);
-    event WithdrawLend(address indexed lender,string token, uint amount);
+    event WithdrawLend(address indexed lender, string token, uint amount);
     event ClaimYield(address indexed lender, uint amount);
     event Collateralize(address indexed borrower, uint amount);
     event WithdrawCollateral(address indexed borrower, uint amount);
@@ -44,7 +44,7 @@ contract ProtocolCore is Ownable{
     ///@notice initiating tokens
     ///@param _baseAssetAddress address of base asset token
     ///@param _sonkAddress address of $FUSN token
-    constructor(IERC20 _baseAssetAddress, IERC20 _ethAssetAddress, SonkToken _sonkAddress,address _ethAggregatorAddress,address _sonicAggregatorAddress) {
+    constructor(IERC20 _baseAssetAddress, IERC20 _ethAssetAddress, SonkToken _sonkAddress, address _ethAggregatorAddress, address _sonicAggregatorAddress) {
         baseAsset = _baseAssetAddress;
         ethAsset = _ethAssetAddress;
         sonkToken = _sonkAddress;
@@ -56,13 +56,13 @@ contract ProtocolCore is Ownable{
     ///@dev added 'virtual' identifier for MockCore to override
     modifier passedLiquidation(address _borrower) virtual {
 //        uint collatAssetPrice = getCollatAssetPrice('Sonic')/getCollatAssetPrice('ETH');
-        require(getCollatAssetPrice(sonicAggregatorAddress)/getCollatAssetPrice(ethAggregatorAddress) * collateralBalance[_borrower]  <= calculateLiquidationPoint(_borrower), "Position can't be liquidated!");
+        require(collateralBalance[_borrower] * getCollatAssetPrice(sonicAggregatorAddress) / getCollatAssetPrice(ethAggregatorAddress) <= calculateLiquidationPoint(_borrower), "Position can't be liquidated!");
         _;
     }
 
     ///@notice Function to get latest price of ETH in USD
     ///@return collatAssetPrice price of ETH in USD
-    function getCollatAssetPrice(address aggregatorAddress ) public view returns(uint collatAssetPrice){
+    function getCollatAssetPrice(address aggregatorAddress) public view returns (uint collatAssetPrice){
         (,int price,,,) = AggregatorV3Interface(aggregatorAddress).latestRoundData();
         collatAssetPrice = uint(price);
     }
@@ -70,7 +70,7 @@ contract ProtocolCore is Ownable{
     ///@notice calculates amount of time the lender has been lending since the last update.
     ///@param _lender address of lender
     ///@return lendingTime amount of time staked by lender
-    function calculateYieldTime(address _lender) public view returns(uint lendingTime) {
+    function calculateYieldTime(address _lender) public view returns (uint lendingTime) {
         lendingTime = block.timestamp - startTime[_lender];
     }
 
@@ -78,19 +78,19 @@ contract ProtocolCore is Ownable{
     ///@dev rate = timeStaked / amount of time needed to earn 100% of $FUSN tokens. 31536000 = number of seconds in a year.
     ///@param _lender address of lender
     ///@return yield amount of $FUSN tokens earned by lender
-    function calculateYieldTotal(address _lender) public view returns(uint yield) {
-        uint timeStaked = calculateYieldTime(_lender) ;
+    function calculateYieldTotal(address _lender) public view returns (uint yield) {
+        uint timeStaked = calculateYieldTime(_lender);
         yield = lendingBalance[_lender] * timeStaked / 31536000;
     }
 
     ///@notice calculates the borrow limit depending on the price of ETH and borrow limit rate.
     ///@return limit current borrow limit for user
-    function calculateBorrowLimit(address _borrower) public view returns(uint limit) {
+    function calculateBorrowLimit(address _borrower) public view returns (uint limit) {
 //        uint collatAssetPrice = getCollatAssetPrice('Sonic')/getCollatAssetPrice('ETH');
-        limit = ((((getCollatAssetPrice(sonicAggregatorAddress)/getCollatAssetPrice(ethAggregatorAddress) * collateralBalance[_borrower]) * 70) / 100)) - borrowBalance[_borrower];
+        limit = (collateralBalance[_borrower] * getCollatAssetPrice(ethAggregatorAddress) * 70 / 100 / getCollatAssetPrice(sonicAggregatorAddress)) - borrowBalance[_borrower];
     }
 
-    function calculateLiquidationPoint(address _borrower) public view returns(uint point) {
+    function calculateLiquidationPoint(address _borrower) public view returns (uint point) {
         point = borrowBalance[_borrower] + (borrowBalance[_borrower] * 10) / 100;
     }
 
@@ -100,7 +100,7 @@ contract ProtocolCore is Ownable{
         require(_amount > 0, "Can't lend amount: 0!");
         require(baseAsset.balanceOf(msg.sender) >= _amount, "Insufficient balance!");
 
-        if(isLending[msg.sender]) {
+        if (isLending[msg.sender]) {
             uint yield = calculateYieldTotal(msg.sender);
             sonkBalance[msg.sender] += yield;
         }
@@ -129,30 +129,30 @@ contract ProtocolCore is Ownable{
         _amount = 0;
         lendingBalance[msg.sender] -= withdrawAmount;
 
-        if(lendingBalance[msg.sender] == 0){
+        if (lendingBalance[msg.sender] == 0) {
             isLending[msg.sender] = false;
         }
 
-        if(baseAsset.balanceOf(address(this)) >= withdrawAmount){
+        if (baseAsset.balanceOf(address(this)) >= withdrawAmount) {
             require(baseAsset.transfer(msg.sender, withdrawAmount), "Transaction failed!");
-            emit WithdrawLend(msg.sender, 'S',withdrawAmount);
+            emit WithdrawLend(msg.sender, 'S', withdrawAmount);
         }
         else {
             //when the amount of base asset is not enough, we need to convert to ETH
-            withdrawAmount = getCollatAssetPrice(ethAggregatorAddress)/getCollatAssetPrice(sonicAggregatorAddress)*withdrawAmount;
+            withdrawAmount = withdrawAmount * getCollatAssetPrice(ethAggregatorAddress) / getCollatAssetPrice(sonicAggregatorAddress);
             require(ethAsset.transfer(msg.sender, withdrawAmount), "No sufficient fund in the pool!");
-            emit WithdrawLend(msg.sender, 'ETH',withdrawAmount);
+            emit WithdrawLend(msg.sender, 'ETH', withdrawAmount);
         }
 
     }
-    
+
     ///@notice claims all yield earned by lender.
     function claimYield() external {
         uint yield = calculateYieldTotal(msg.sender);
 
         require(yield > 0 || sonkBalance[msg.sender] > 0, "No, $Sonk tokens earned!");
 
-        if(sonkBalance[msg.sender] != 0) {
+        if (sonkBalance[msg.sender] != 0) {
             uint oldYield = sonkBalance[msg.sender];
             sonkBalance[msg.sender] = 0;
             yield += oldYield;
@@ -204,16 +204,16 @@ contract ProtocolCore is Ownable{
 
         emit Borrow(msg.sender, _amount);
     }
-    
+
     ///@notice repays base asset debt
     ///@param _amount amount of base asset to repay
     function repay(uint _amount) external {
         require(isBorrowing[msg.sender], "Can't repay before borrowing!");
         require(baseAsset.balanceOf(msg.sender) >= _amount, "Insufficient funds!");
-        require(_amount > 0 , "Can't repay amount 0!");
+        require(_amount > 0, "Can't repay amount 0!");
         require(_amount <= borrowBalance[msg.sender], "Can't repay amount more than borrowed!");
 
-        if(_amount == borrowBalance[msg.sender]){ 
+        if (_amount == borrowBalance[msg.sender]) {
             isBorrowing[msg.sender] = false;
         }
 
@@ -231,9 +231,9 @@ contract ProtocolCore is Ownable{
     ///@dev liquidationReward 1.25% of borrower's ETH collateral
     function liquidate(address _borrower) external onlyOwner passedLiquidation(_borrower) {
         require(isBorrowing[_borrower], "This address is not borrowing!");
-        require(msg.sender != _borrower, "Can't liquidated your own position!");    
+        require(msg.sender != _borrower, "Can't liquidated your own position!");
 
-        uint liquidationReward = (collateralBalance[_borrower] * 125) / 10000; 
+        uint liquidationReward = (collateralBalance[_borrower] * 125) / 10000;
 
         collateralBalance[_borrower] = 0;
         borrowBalance[_borrower] = 0;
@@ -248,43 +248,42 @@ contract ProtocolCore is Ownable{
     }
 
     ///@notice returns lending status of lender
-    function getLendingStatus(address _lender) external view returns(bool){
+    function getLendingStatus(address _lender) external view returns (bool){
         return isLending[_lender];
-    }  
+    }
 
     ///@notice retuns amount of $FUSN tokens earned
-    function getEarnedSonkTokens(address _lender) external view returns(uint){
+    function getEarnedSonkTokens(address _lender) external view returns (uint){
         return sonkBalance[_lender] + calculateYieldTotal(_lender);
     }
 
     ///@notice returns amount of base asset lent
-    function getLendingBalance(address _lender) external view returns(uint){
+    function getLendingBalance(address _lender) external view returns (uint){
         return lendingBalance[_lender];
     }
 
     ///@notice returns amount of collateralized asset
-    function getCollateralBalance(address _borrower) external view returns(uint){
+    function getCollateralBalance(address _borrower) external view returns (uint){
         return collateralBalance[_borrower];
     }
 
-
     ///@notice returns borrowing status of borrower
-    function getBorrowingStatus(address _borrower) external view returns(bool){
+    function getBorrowingStatus(address _borrower) external view returns (bool){
         return isBorrowing[_borrower];
     }
 
     ///@notice returns amount of base asset borrowed
-    function getBorrowBalance(address _borrower) external view returns(uint){
+    function getBorrowBalance(address _borrower) external view returns (uint){
         return borrowBalance[_borrower];
     }
 
     ///@notice returns amount of base asset available to borrow
-    function getBorrowLimit(address _borrower) external view returns(uint){
+    function getBorrowLimit(address _borrower) external view returns (uint){
         return calculateBorrowLimit(_borrower);
     }
 
     ///@notice returns liquidation point
-    function getLiquidationPoint(address _borrower) external view returns(uint){
+    function getLiquidationPoint(address _borrower) external view returns (uint){
         return calculateLiquidationPoint(_borrower);
     }
 }
